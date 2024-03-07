b287e0706da39693bb62f7162b91cf2bbf7aea2a
Yuriy Chepel <yuriy.chepel@gmail.com>
2024-03-07 19:41:51 +0100

    added loader and json data source

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+const loaderNode = document.getElementById('loader');
+const cityCardNode = document.getElementById('cityCard');
 const countryNode = document.getElementById('country');
 const cityNode = document.getElementById('city');
 const temperatureNode = document.getElementById('temperature');
@@ -7,20 +9,29 @@ const weatherImageNode = document.getElementById('weatherIcon');
 const timeNode = document.getElementById('time');
 
 async function fetchWeather() {
-  const resGeo = await fetch(
-    "https://get.geojs.io/v1/ip/geo.json"
-  );
-  const dataGeo = await resGeo.json();
-  const {latitude, longitude, city, country} = dataGeo;
+  // const resGeo = await fetch(
+  //   "https://get.geojs.io/v1/ip/geo.json"
+  // );
+  // const dataGeo = await resGeo.json();
+  // const {latitude, longitude, city, country} = dataGeo;
 
-  const resWeather = await fetch(
-    `https://api.weatherapi.com/v1/current.json?key=13974d32ad974aa1871200231240603&q=${latitude},${longitude}&aqi=no`
-    // `https://api.open-meteo.com/v1/forecast?latitude=${latitude}&longitude=${longitude}&current_weather=true`
-  );
-  const dataWeather = await resWeather.json();
-  console.log(dataWeather);
-  const {temp_c, wind_kph, condition} = dataWeather.current;
-  const {text, icon} = condition;
+  // const resWeather = await fetch(
+  //   `https://api.weatherapi.com/v1/current.json?key=13974d32ad974aa1871200231240603&q=${latitude},${longitude}&aqi=no`
+  // );
+  // const dataWeather = await resWeather.json();
+  // console.log(dataWeather);
+  // const {temp_c, wind_kph, condition} = dataWeather.current;
+  // const {text, icon} = condition;
+
+  loaderNode.style.display = "none";
+  cityCardNode.style.display = "block";
+
+  country = 'Germany';
+  city = 'Plauen';
+  temp_c = 2;
+  wind_kph = 13;
+  text = 'Partly cloudy';
+  icon = 'http://cdn.weatherapi.com/weather/64x64/day/116.png';
 
   timeNode.textContent = getCurrentTime();
   countryNode.textContent = country;
@@ -41,4 +52,27 @@ function getCurrentTime() {
     return formattedTime;
 }
 
-fetchWeather();
+async function test() {
+  const url = 'https://map-places.p.rapidapi.com/nearbysearch/json?location=-33.8670522%2C151.1957362&radius=1500&keyword=cruise&type=restaurant';
+const options = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': '6deac19c98msh693a20409929614p1802bcjsn3cbe9dc936f4',
+		'X-RapidAPI-Host': 'map-places.p.rapidapi.com'
+	}
+};
+
+try {
+	const response = await fetch(url, options);
+	const result = await response.text();
+	console.log(result);
+} catch (error) {
+	console.error(error);
+}
+}
+
+setTimeout(() => {
+  fetchWeather()
+}, 1500);
+
+test();
